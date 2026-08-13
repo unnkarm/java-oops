cd7b4ded16576dfa1471b261dc83c6eb741c4676
unnkarm <unnitakarmakar2005@gmail.com>
2026-08-13 19:46:54 +0530

    datatype

---
+++ Datatype.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Datatype{
+    public static void main(String[] args){
+        String firstEssay= new String("This is my first essay.");
+        Date today= new Date();
+
+        System.out.println("String: "+firstEssay);
+        System.out.println("Date: "+today);
+    }
+}
+
