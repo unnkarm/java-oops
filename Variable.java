8b5d6e0b67f604e4ad1c2cfcdf1b427455c36b06
unnkarm <unnitakarmakar2005@gmail.com>
2026-08-13 19:39:26 +0530

    variable

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable { //file name=class name
+    public static void main(String[] args) {
+        int a = 10; //variable declaration and initialization
+        double b = 20.5; //variable declaration and initialization
+        String c = "Hello"; //variable declaration and initialization
+        long phoneNumber = 1234567890L; //variable declaration and initialization
+        float d = 5.5f; //variable declaration and initialization
+        boolean isJavaFun = true; //variable declaration and initialization
+
+        System.out.println("Integer value: " + a);
+        System.out.println("Double value: " + b);
+        System.out.println("String value: " + c);
+        System.out.println("Long value: " + phoneNumber);
+        System.out.println("Float value: " + d);
+        System.out.println("Boolean value: " + isJavaFun);
+    }
+}
