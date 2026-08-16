dd255036704a58ae033f77fa42e575668b0a1fec
unnkarm <unnitakarmakar2005@gmail.com>
2026-08-16 18:37:09 +0530

    strings

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings { //file name=class name
+    public static void main(String[] args) {
+        String str1 = "Hello"; //variable declaration and initialization
+        String str2 = "World"; //variable declaration and initialization
+        String str3 = str1 + " " + str2; //concatenation of strings
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        System.out.println("Concatenated String: " + str3);
+    }
+}
