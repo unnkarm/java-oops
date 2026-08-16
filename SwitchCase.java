1065b0da3ce384f20a635c232ee2d8a9ee4e76e4
unnkarm <unnitakarmakar2005@gmail.com>
2026-08-16 20:10:20 +0530

    if-else and switch-case

---
+++ SwitchCase.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (1-3): ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        sc.close();
+    }
+}
