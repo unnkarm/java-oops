36ee6ca6412db6f32603d693d2b392f0ad92956a
unnkarm <unnitakarmakar2005@gmail.com>
2026-08-16 20:15:25 +0530

    while loop

---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop{
+    public static void main(String[] args){
+        int a=5;
+        while(a<10){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
+
+// Initialization -> Condition -> Increment/Decrement
